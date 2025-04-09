74df36eba6f02009b4230bff8760a6088662daf6
BoilTask <BoilTask@qq.com>
2025-04-09 20:47:19 +0800

    更新测试逻辑

--- src/index.ts
+++ src/index.ts
@@ -76,48 +76,8 @@ export default {
     // 3. Upload to GitHub via API
     const GITHUB_TOKEN = env.GITHUB_TOKEN;
 
-    const REPO_OWNER = "BoilTask";
-    const REPO_NAME = "boiltask-oj";
-    const FILE_PATH = `content/problem/${targetOj}/${targetProblem}/index.md`; // 文件路径
-    const COMMIT_MESSAGE = `add problem ${targetOj}-${targetProblem} ${title}`;
 
-    const fileMetadataRes = await fetch(
-      `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-          "User-Agent": "Cloudflare-Worker",
-        },
-      }
-    );
-    const fileMetadata = await fileMetadataRes.json();
-	const fileSha = fileMetadata.sha; // 获取 SHA 值
-
-    const uploadRes = await fetch(
-      `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`,
-      {
-        method: "PUT",
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-          "Content-Type": "application/json",
-          "User-Agent": "Cloudflare-Worker",
-        },
-        body: JSON.stringify({
-          message: COMMIT_MESSAGE,
-          content: btoa(unescape(encodeURIComponent(markdown))), // base64 编码
-          sha: fileSha, // 提供当前文件的 SHA 值
-        }),
-      }
-    );
-
-    const uploadResult = await uploadRes.json();
-
-    return new Response(
-      JSON.stringify({ ok: true, upload: uploadResult }, null, 2),
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+	return new Response(JSON.stringify(env));
+	
   },
 };
