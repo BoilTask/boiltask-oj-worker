8fe8409d442727d4127281edf8ff3e968a379c02
BoilTask <BoilTask@qq.com>
2025-04-09 20:08:47 +0800

    完善nyoj爬虫

---
+++ assets/template/nyoj.md
@@ -0,0 +1,27 @@
+---
+title: 【{{oj_title}}】[{{problem}}]{{title}}
+type: post
+slug: {{oj}}-{{problem}}
+categories:
+  - {{oj}}
+---
+
+# {{title}}
+
+## 题目描述
+{{description}}
+
+## 输入描述
+{{input}}
+
+## 输出描述
+{{output}}
+
+## 示例
+{{examples}}
+
+## 提示
+{{hint}}
+
+## 来源
+{{source}}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,76 @@
+import TurndownService from "turndown";
+import { createDocument } from "@mixmark-io/domino";
+const turndownService = new TurndownService();
+
+const getOjTitle = (oj: string): string => {
+  switch (oj) {
+    case "nyoj":
+      return "NYOJ";
+    default:
+      return oj.toUpperCase();
+  }
+};
+
+function render(template: string, data: Record<string, string>): string {
+  return template.replace(/\{\{(\w+)\}\}/g, (_, key) => data[key] ?? "");
+}
+
+function decodeHTML(html: string): string {
+  return turndownService.turndown(createDocument(html));
+}
+
 export default {
-  async fetch(request, env, ctx) {
-    return new Response(`已上传 Markdown 到 GitHub:`);
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const { searchParams } = new URL(request.url);
+    const targetOj = searchParams.get("oj");
+    if (!targetOj) {
+      return new Response("Missing 'oj' parameter", { status: 400 });
+    }
+    const targetProblem = searchParams.get("problem");
+    if (!targetProblem) {
+      return new Response("Missing 'problem' parameter", { status: 400 });
+    }
+    if (targetOj !== "nyoj") {
+      return new Response("Unsupported OJ", { status: 400 });
+    }
+
+    const targetUrl =
+      "https://xcpc.nyist.edu.cn/api/get-problem-detail?problemId=" +
+      targetProblem;
+
+    // 1. Fetch JSON data
+    const res = await fetch(targetUrl);
+    const json = await res.json();
+
+    if (!json?.data?.problem) {
+      return new Response("Invalid problem format", { status: 500 });
+    }
+
+    const assetUrl = new URL("/template/nyoj.md", request.url);
+    const templateResp = await env.ASSETS.fetch(assetUrl);
+    const templateText = await templateResp.text();
+
+    const { title, description, input, output, examples, source, hint } =
+      json.data.problem;
+
+    const markdown = render(templateText, {
+      oj: targetOj,
+      problem: targetProblem,
+      oj_title: getOjTitle(targetOj),
+      title: decodeHTML(title),
+      description: decodeHTML(description),
+      input: decodeHTML(input),
+      output: decodeHTML(output),
+      examples: decodeHTML(examples),
+      source: decodeHTML(source),
+      hint: decodeHTML(hint) || "无",
+    });
+
+    return new Response(markdown);
   },
 };
